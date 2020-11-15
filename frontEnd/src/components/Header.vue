ad8a128b9764a9b5b2a56049feb67c90b7965b49
rulan.lu1 <rulan.lu1@cs.ucalgary.ca>
2020-11-14 19:26:07 -0700

    added profile information to header

--- frontEnd/src/components/Header.vue
+++ frontEnd/src/components/Header.vue
@@ -1,5 +1,6 @@
 <template>
     <v-app-bar
+        data-app
         color="primary"
         padding-left="1rem"
         padding-right="1rem"
@@ -11,12 +12,59 @@
         
         <v-spacer></v-spacer>
 
-        <v-btn rounded elevation="0">
-            Mr. Worldwide
+        <!-- <v-btn rounded elevation="0">
+            Tim Apple
             <v-icon right>mdi-account</v-icon>
-        </v-btn>
-        <!-- add name next to icon -->
-        
+        </v-btn> -->
+        <v-menu
+            v-model="menu"
+            :close-on-content-click="false"
+            :close-on-click="true"
+            :nudge-width="200"
+            offset-x
+        >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            v-bind="attrs"
+            v-on="on"
+            rounded 
+            elevation="0"
+          >
+            Steve Buscemi
+            <v-icon right>mdi-account</v-icon>
+          </v-btn>
+        </template>
+  
+        <v-card>
+          <v-list>
+            <v-list-item>
+  
+              <v-list-item-content>
+                <v-list-item-title align="left">Profile Information</v-list-item-title>
+              </v-list-item-content>
+              <v-list-item-action>
+                <v-btn
+                  align="right"
+                  icon
+                  @click="menu = false"
+                >
+                  <v-icon small>mdi-close</v-icon>
+                </v-btn>
+              </v-list-item-action>
+ 
+            </v-list-item>
+            <v-divider></v-divider>
+            <v-list-item>
+              <v-list-item-content>
+              <v-list-item-subtitle align="left">Steve Buscemi</v-list-item-subtitle>
+              <v-list-item-subtitle align="left">Community Mentor</v-list-item-subtitle>
+                <v-list-item-subtitle align="left">steve@gmail.com</v-list-item-subtitle>
+                </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-card>
+      </v-menu>
+
         <v-btn 
             text
             rounded
@@ -29,6 +77,9 @@
 </template>
 
 <script>
-  export default {
-  }
+    export default {
+        data: () => ({
+            menu: false,
+        }),
+    }
 </script>
