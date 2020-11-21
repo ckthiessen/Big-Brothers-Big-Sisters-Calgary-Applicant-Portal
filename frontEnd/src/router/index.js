73c26e55c04bd30e915cbb9ba621b3d86ecee9a0
velvetarchange <h.dastidar@gmail.com>
2020-11-20 20:47:24 -0700

    BBBSWA-45 Updated routes with dynamic routes and routeguards. Added some potentially useful firebase logic but unsure if it is correct

--- frontEnd/src/router/index.js
+++ frontEnd/src/router/index.js
@@ -5,8 +5,13 @@ import Signin from '../views/Signin.vue'
 import Forgot from '../views/Forgot.vue'
 import Administrator from '../views/Administrator.vue'
 import ApplicantPortal from '../views/ApplicantPortal.vue'
-// import AdminApplicant from '../views/AdminApplicant.vue'
+import AdminApplicant from '../views/AdminApplicant.vue'
 
+//import firebase
+/*
+import * as firebase from 'firebase';
+import 'firebase/auth'
+*/
 Vue.use(VueRouter);
 
 const routes = [
@@ -29,7 +34,25 @@ const routes = [
   // Still need to add the dynamic routes
   {
     path: '/applicant/:id',
-    component: ApplicantPortal
+    component: ApplicantPortal,
+    meta: {requiresAuth: true}
+  },
+  {
+    path: '/admin/home',
+    component: Administrator,
+    meta: {
+      requiresAuth: true,
+      requiresAdminPerm: true
+    }
+  },
+  {
+    path: '/admin/:id',
+    component: AdminApplicant,
+    meta: {
+      requiresAuth: true,
+      requiresAdminPerm: true
+    }
+
   }
 ]
 
@@ -37,5 +60,30 @@ const router = new VueRouter({
   mode: 'history',
   routes
 });
+
+router.beforeEach((to, from, next) => {
+  //Adds this logic to check whether each route is authenticated
+  //this guards users from accessing certain routes ex: Admin/ Applicant dashboard etc
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+  const isAdmin = to.matched.some(record => record.meta.isAdmin);
+  //Add firebase logic here
+  /*
+  const isAuthenticated = firebase.auth().currentUser;
+  const hasAdminPerms = firebase.auth().currentUser.isAdmin === true;
+
+  if(requiresAuth & !isAutheticated){
+    next("/signin");
+  }else{
+    next()
+  }
+
+  if(requiresAdminPerm & !hasAdminPerm) {
+    next("/signin");
+  }else{
+    next()
+  }
+  */
+
+})
 
 export default router
