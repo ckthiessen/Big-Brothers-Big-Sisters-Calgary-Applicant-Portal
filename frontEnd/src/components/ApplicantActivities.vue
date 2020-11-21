3c7dcc1bd168ce0e6a6cfc012fe11d109d5cb7e2
Cole Thiessen <colekthiessen@gmail.com>
2020-11-21 15:23:26 -0700

    BBBSWA-20 Refactored status into map. Fixed button titles. Fixed applicants json

--- frontEnd/src/components/ApplicantActivities.vue
+++ frontEnd/src/components/ApplicantActivities.vue
@@ -1,42 +1,60 @@
 <template>
   <v-card width="90%" class="mx-auto">
     <v-card-title> Activities </v-card-title>
-    <v-container >
+    <v-container>
       <v-row>
         <v-col v-for="header in headers" :key="header"> {{ header }} </v-col>
       </v-row>
       <v-divider></v-divider>
-      <v-expansion-panels accordion focusable >
-        <div v-for="(task, index) in tasks" :key="task.name" width=100% style="width: 1800px">
-          <v-expansion-panel >
+      <v-expansion-panels accordion focusable>
+        <div
+          v-for="(task, index) in tasks"
+          :key="task.name"
+          width="100%"
+          style="width: 1800px"
+        >
+          <v-expansion-panel>
             <v-expansion-panel-header>
-            <v-row>
-              <v-col>
-                {{ task.name }} 
-              </v-col>
-              <v-col>
-                <v-chip text-color="black" class="ma-2" :color="statusColor[task.status]"> 
-                  <v-icon 
-                  class="pl-1"
-                  color="white"
-                  left>
-                    {{ statusIcons[task.status] }}
+              <v-row>
+                <v-col>
+                  {{ task.name }}
+                </v-col>
+                <v-col>
+                  <v-chip
+                    text-color="black"
+                    class="ma-2"
+                    :color="status[task.status].color"
+                  >
+                    <v-icon class="pl-1" color="white" left>
+                      {{ status[task.status].icon }}
+                    </v-icon>
+                    {{ status[task.status].title }}
+                  </v-chip>
+                </v-col>
+                <v-col> {{ task.dueDate }} </v-col>
+                <v-col>
+                  <v-icon color="accent">
+                    {{
+                      task.upload ? uploadIcons.upload : uploadIcons.noUpload
+                    }}
                   </v-icon>
-                  {{ task.status }}  
-                </v-chip>
-              </v-col>
-              <v-col> {{ task.dueDate }} </v-col>
-              <v-col>
-                <v-icon color="accent">
-                  {{ task.upload ? uploadIcons.upload : uploadIcons.noUpload }}
-                </v-icon>
-              </v-col>
-            </v-row>
+                </v-col>
+              </v-row>
             </v-expansion-panel-header>
             <v-expansion-panel-content>
-              <p class="float-left mt-7" style="width: 40%; text-align: left"> {{ task.description }} </p>
-              <v-btn @click="changeStatus(task.status, index)" v-if="!noActions.includes(task.name)" class="float-right mt-5"> Mark Complete </v-btn>
-              <v-btn v-if="task.upload" class="float-right mt-5 mr-5"> Upload Document </v-btn>
+              <p class="float-left mt-7" style="width: 40%; text-align: left">
+                {{ task.description }}
+              </p>
+              <v-btn
+                @click="changeStatus(task.status, index)"
+                v-if="!noActions.includes(task.name)"
+                class="float-right mt-5"
+              >
+                {{ buttonTitle(task.status) }}
+              </v-btn>
+              <v-btn v-if="task.upload" class="float-right mt-5 mr-5">
+                Upload Document
+              </v-btn>
             </v-expansion-panel-content>
           </v-expansion-panel>
           <v-divider></v-divider>
@@ -59,36 +77,54 @@ export default {
     return {
       tasks: require("../assets/activities.json"),
       headers: ["Name", "Status", "Due Date", "Upload", ""],
-      statusColor: {
-        Complete: "accent",
-        Incomplete: "needsattention"
+      status: {
+        Complete: {
+          color: "complete",
+          title: "Complete",
+          icon: "mdi-check-circle",
+        },
+        InProgress: {
+          color: "inprogress",
+          title: "Waiting Approval",
+          icon: "mdi-dots-horizontal-circle",
+        },
+        Incomplete: {
+          color: "needsattention",
+          title: "Incomplete",
+          icon: "mdi-alert",
+        },
       },
       uploadIcons: {
         noUpload: "mdi-cloud-off-outline",
         upload: "mdi-cloud-upload",
         uploadComplete: "mdi-cloud-check",
       },
-      statusIcons: {
-        Complete: "mdi-check", 
-        Incomplete: "mdi-alert"
-      },
       dialog: false,
-      noActions: ["BIG Profile", "You are no BIG Deal :(", "You are a BIG Deal!"]
+      noActions: [
+        "BIG Profile",
+        "You are no BIG Deal :(",
+        "You are a BIG Deal!",
+      ],
     };
   },
-  created() {},
-  computed: {
-    // showIcon: () => {
-    //   return this.
-    // }
+  props: {
+    applicant: Object,
   },
+  // created() {},
+  // computed: {
+  // },
   methods: {
     changeStatus(status, index) {
       let selectedTask = this.tasks[index];
-      if(selectedTask.status === "Incomplete") {
-        selectedTask.status = "Complete"
+      if (selectedTask.status === "Incomplete") {
+        selectedTask.status = "InProgress";
+      } else if (selectedTask.status === "InProgress") {
+        selectedTask.status = "Incomplete";
       }
-    }
+    },
+    buttonTitle: function (status) {
+      return status === "InProgress" ? "Mark Incomplete" : "Request Approval"
+    },
   },
 };
 </script>
