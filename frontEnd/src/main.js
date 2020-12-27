830c06d9d0bad880435e715728116d2ee9eb81d1
velvetarchange <h.dastidar@gmail.com>
2020-12-27 16:17:28 -0700

    BBBSWA-54 rollover auth to firebase

--- frontEnd/src/main.js
+++ frontEnd/src/main.js
@@ -2,14 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
-// import VueCookies from 'vue-cookies'
 import "./firebase"
 import { auth } from "./firebase";
 
 //Disable "You are running Vue in development mode" warning
 Vue.config.productionTip = false;
-
-// Vue.use(VueCookies);
 //this will prevent user from being logged out on page refresh
 let app;
 
@@ -18,7 +15,6 @@ auth.onAuthStateChanged(() => {
     app = new Vue({
       vuetify,
       router,
-      // VueCookies,
       render: h => h(App)
     }).$mount('#app')
   }
