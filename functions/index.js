2b505a442293725946872cbb419dd054a5583463
Cole Thiessen <colekthiessen@gmail.com>
2021-01-02 22:28:42 -0700

    BBBSWA-92 Wrote firebase function to update user tasks

--- functions/index.js
+++ functions/index.js
@@ -28,23 +28,35 @@ exports.getUserByID = functions.https.onCall((data, context) => {
       .then(doc => resolve(doc.data())) // Successful, resolve with user document
       .catch(() => reject(new functions.https.HttpsError("internal", "Could not get user"))); // Failed, reject promise with HTTP error message
   });
-}); 
+});
 
 exports.createUser = functions.https.onCall((data, context) => {
   let userInfo = data;
   userInfo.tasks = taskFactory.getDefaultTasks();
   userInfo.notifications = [
-      {
-        message: "Congratulations on making your account!",
-        date: new Date().toLocaleDateString("en-CA", { timeZone: "America/Edmonton" })
-      }
+    {
+      message: "Congratulations on making your account!",
+      date: new Date().toLocaleDateString("en-CA", { timeZone: "America/Edmonton" })
+    }
   ];
   userInfo.isAdmin = false;
   userInfo.isCommunityMentor = false;
-  userInfo.requiresHomeAssessment =  false;
+  userInfo.requiresHomeAssessment = false;
   return new Promise((resolve, reject) => {
     db.collection('users').doc(data.id).set(userInfo)
       .then(() => resolve()) // Successful, resolve with nothing
-      .catch(() => reject(new functions.https.HttpsError("internal", "Could not update user type"))); // Failed, reject promise with HTTP error message
+      .catch(() => reject(new functions.https.HttpsError("internal", "Could not create user"))); // Failed, reject promise with HTTP error message
   });
-})
+});
+
+exports.updateTask = functions.https.onCall((data, context) => {
+  if (!context.auth.uid) { throw new functions.https.HttpsError("unauthenticated", "User not authenticated"); }
+  let id = data.id;
+  return new Promise((resolve, reject) => {
+    db.collection('users').doc(id).update({
+      "tasks": data.newTasks
+    })
+      .then(() => resolve()) // Successful, resolve with nothing
+      .catch(() => reject(new functions.https.HttpsError("internal", "Could not update task"))); // Failed, reject promise with HTTP error message
+  });
+});
